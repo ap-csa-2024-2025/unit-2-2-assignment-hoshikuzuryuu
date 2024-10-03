3df31afc5ab3bb203942799343f674aa21ac2295
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-10-03 10:18:40 -0700

    yes

---
+++ blahblahblah.java
@@ -0,0 +1,15 @@
+public class blahblahblah
+{
+    public static void main(String[] args)
+    {
+        String mySchool = "WHS";
+        int l = 3;
+        int w = 5; 
+        double area = l*w;
+        double perimeter = (2*(l+w));
+        System.out.println("The perimeter is " + perimeter);
+        System.out.println("The area is " + area);
+        
+    }
+}
+   
